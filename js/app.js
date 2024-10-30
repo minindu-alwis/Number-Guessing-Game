07cfa7dc4bb09f4fd92993a4826b04d4c44d0160
minindu-alwis <minindualwis@gmail.com>
2024-10-30 11:22:36 +0530

    Guessing Game 2

--- js/app.js
+++ js/app.js
@@ -1,8 +1,49 @@
+
+let randomnumber=Math.floor((Math.random()*10)+1);
+console.log(randomnumber);
+
+let chancess=3;
+
 function guess(){
     
-    let randomnumber=Math.floor((Math.random()*10)+1);
-    console.log(randomnumber);
+
 
     let userinput=document.getElementById("userinputt").value;
     console.log(userinput);
+
+    let numbersleft=document.getElementById("leftnumers");
+    let winorlost=document.getElementById("winorlost");
+
+    let coolorhot=document.getElementById("coolorhot");
+     
+
+    if(chancess>0){
+
+        if(randomnumber==userinput){
+
+            numbersleft.innerHTML=("🎈 Changes Left : 0 🎈");
+            winorlost.innerHTML=("🔥 Win the Game 🔥");
+            coolorhot.innerHTML=("💞💞 Gessing Number Is Correct !!!!!!!!!!!! 💞💞");
+            alert("ගැම්මක් සුද්දා 💦");
+
+        }else{
+            chancess--;
+            numbersleft.innerHTML=(`Changes Left : ${chancess} `);
+            if(randomnumber>userinput){
+                coolorhot.innerHTML=("Your Numeber is  Low 😂");
+            }else{
+                coolorhot.innerHTML=("Your Numeber is  Higher 😂");
+            }
+            winorlost.innerHTML=("Try Agin Broooooo 😴😴🥱😫😪");
+        }
+    }
+    if(chancess === 0 && randomnumber!= userinput){
+        numbersleft.innerHTML=("Changes Left : 0");
+        coolorhot.innerHTML=("Bad Luck !!!! 😝");
+        winorlost.innerHTML=(`💕 Lost the Game !! Correct Number is = ${randomnumber} 💕`);
+    }
+
+    
+
 }
+
