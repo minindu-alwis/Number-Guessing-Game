b4bbde81639237d50ddc83672475873384deadaa
minindu-alwis <minindualwis@gmail.com>
2024-10-30 09:06:16 +0530

    Guessing Game

---
+++ js/app.js
@@ -0,0 +1,8 @@
+function guess(){
+    
+    let randomnumber=Math.floor((Math.random()*10)+1);
+    console.log(randomnumber);
+
+    let userinput=document.getElementById("userinputt").value;
+    console.log(userinput);
+}
